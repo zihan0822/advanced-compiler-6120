e5e0bd1decf217fe196a9d6d2984f0f2d8e92e36
Zihan <noah_822@163.com>
2025-03-02 13:47:08 -0500

    set/get transform
    TODO: not ssa in some cases
    
    steps:
    1. find live-in variables with worklist algorithm for all blk and which blk each of the reference comes from
    2. for all dom frontier, register all live-in vars as may shadow
    3. recurse on dom tree, insert get inst in current blk, do neccessary
       renaming, and insert set for successor blk
    
    The main difference between last iteration is that:
    we use worklist algorithm to find accumulated live-in in each blk instead of local live in,
    but this will introduce unnecessary get instr which leads to non-ssa form

--- bril-rs/src/transform/ssa.rs
+++ bril-rs/src/transform/ssa.rs
@@ -1,16 +1,61 @@
 use crate::analyzer::dom::{DomNodeRef, DomTree};
 use crate::bril::LabelOrInst;
 use crate::cfg::prelude::*;
-use crate::optim::dce::global::LivenessAnalysis;
 use crate::optim::dflow::WorkListAlgo;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::default::Default;
-use std::sync::{Arc, Weak};
+use std::sync::{Arc, Mutex, Weak};
 
-pub fn cfg_into_ssa(cfg: Cfg, func_ctx: FuncCtx) -> Cfg {
+pub fn cfg_into_ssa(mut cfg: Cfg, func_ctx: FuncCtx) -> Cfg {
+    if let Some(dummy_entry_blk) = require_dummy_entry_blk(&cfg, &func_ctx) {
+        let old_root_cfg_node = Weak::clone(&cfg.root);
+        let new_root_cfg_node = Arc::new(Mutex::new(CfgNode {
+            label: None,
+            blk: dummy_entry_blk,
+            successors: vec![old_root_cfg_node.clone()],
+            predecessors: vec![],
+        }));
+        old_root_cfg_node
+            .upgrade()
+            .unwrap()
+            .lock()
+            .unwrap()
+            .predecessors
+            .push(Arc::downgrade(&new_root_cfg_node));
+        cfg.root = Arc::downgrade(&new_root_cfg_node);
+        cfg.nodes.insert(0, new_root_cfg_node);
+    }
+
     let mut ssa_ctx = SSATransContext::new(&cfg, &func_ctx);
     ssa_ctx.walk_cfg();
     cfg
+}
+fn require_dummy_entry_blk(cfg: &Cfg, func_ctx: &FuncCtx) -> Option<BasicBlock> {
+    let root_node = cfg.root.upgrade().unwrap();
+    let root_node_lock = root_node.lock().unwrap();
+    if root_node_lock.label.is_some() && func_ctx.args.is_some() {
+        let mut instrs = vec![];
+        for arg in func_ctx.args.as_ref().unwrap() {
+            instrs.push(
+                serde_json::from_str(&format!(
+                    r#"{{
+                        "dest": "{0}",
+                        "args": ["{0}"],
+                        "op": "id",
+                        "type": "{1}" 
+                    }}"#,
+                    &arg.name, &arg.ty
+                ))
+                .unwrap(),
+            );
+        }
+        Some(BasicBlock {
+            label: None,
+            instrs,
+        })
+    } else {
+        None
+    }
 }
 
 struct SSATransContext<'a> {
@@ -18,7 +63,6 @@ struct SSATransContext<'a> {
     func_ctx: &'a FuncCtx,
     dom_tree: DomTree,
     cfg_ptr2dom_node: HashMap<NodePtr, DomNodeRef>,
-    cfg_ptr2cfg_node: HashMap<NodePtr, NodeRef>,
     blk_cache: HashMap<NodePtr, PerBlockCache>,
 }
 
@@ -27,50 +71,42 @@ struct PerBlockCache {
     // original-new -> new-name
     renamed_live_in: HashMap<String, String>,
     renamed_live_out: HashMap<String, String>,
-    liveness_vars: HashSet<String>,
+    acc_live_in: HashMap<String, String>,
     live_in_ty: HashMap<String, String>,
-    live_in_may_shadow: HashSet<String>,
+    live_in_may_shadow: HashMap<String, String>,
 }
 
 impl<'a> SSATransContext<'a> {
     fn new(cfg: &'a Cfg, func_ctx: &'a FuncCtx) -> Self {
         let mut cfg_ptr2dom_node = HashMap::new();
-        let mut cfg_ptr2cfg_node = HashMap::new();
         let dom_tree = DomTree::from_cfg(cfg);
         for dom_node in &dom_tree.nodes {
             let dom_node_lock = dom_node.lock().unwrap();
             let cfg_ptr = Arc::as_ptr(&dom_node_lock.cfg_node);
-            cfg_ptr2cfg_node.insert(cfg_ptr, Arc::clone(&dom_node_lock.cfg_node));
-            cfg_ptr2dom_node.insert(cfg_ptr, Arc::clone(&dom_node));
+            cfg_ptr2dom_node.insert(cfg_ptr, Arc::clone(dom_node));
         }
         Self {
             cfg,
             func_ctx,
             dom_tree,
             cfg_ptr2dom_node,
-            cfg_ptr2cfg_node,
             blk_cache: Default::default(),
         }
     }
 
     fn walk_cfg(&mut self) {
+        // add dummy entry block if the current entry block can be the target of jump
+        self.update_liveness_cache();
         self.per_blk_var_renaming();
         self.inspect_dom_frontier();
         self.insert_set_and_get();
     }
 
     fn update_liveness_cache(&mut self) {
-        let liveness_ret = LivenessAnalysis.execute(&self.cfg);
-        for node in &self.cfg.nodes {
-            let in_flows: HashSet<String> = LivenessAnalysis::predecessors(node)
-                .iter()
-                .flat_map(|pred| liveness_ret.get(&Arc::as_ptr(pred)).unwrap().clone())
-                .collect();
-            let defs = node.lock().unwrap().blk.defs();
-            self.blk_cache
-                .entry(Arc::as_ptr(node))
-                .or_default()
-                .liveness_vars = in_flows.intersection(&defs).cloned().collect();
+        let liveness_ret = LivenessAnalysisWithLabelBackProp.execute(self.cfg);
+        for (node_ptr, liveness_vars) in liveness_ret {
+            let node_cache = self.blk_cache.entry(node_ptr).or_default();
+            node_cache.acc_live_in = liveness_vars;
         }
     }
 
@@ -86,19 +122,19 @@ impl<'a> SSATransContext<'a> {
             args_ty: func_args_and_ty,
         };
 
-        let per_blk_live_in_types = live_in_type_algo.execute(&self.cfg);
+        let per_blk_live_in_types = live_in_type_algo.execute(self.cfg);
 
         for node in &self.cfg.nodes {
             let cfg_ptr = Arc::as_ptr(node);
             let mut cfg_lock = node.lock().unwrap();
 
-            let mut blk_live_in_types = VarTypeAnalysis::merge(
+            let blk_live_in_types = VarTypeAnalysis::merge(
                 cfg_lock
                     .predecessors
                     .iter()
                     .map(|pred| {
                         per_blk_live_in_types
-                            .get(&Weak::as_ptr(&pred))
+                            .get(&Weak::as_ptr(pred))
                             .unwrap()
                             .clone()
                     })
@@ -111,7 +147,6 @@ impl<'a> SSATransContext<'a> {
             let blk_cache = self.blk_cache.entry(cfg_ptr).or_default();
             local_rename_ctx.rename_local_vars();
             let (renamed_live_in, renamed_live_out) = local_rename_ctx.live_in_and_out_renaming();
-            blk_live_in_types.retain(|var, _| renamed_live_in.contains_key(var));
             blk_cache.live_in_ty = blk_live_in_types;
             blk_cache.renamed_live_in = renamed_live_in;
             blk_cache.renamed_live_out = renamed_live_out;
@@ -122,18 +157,11 @@ impl<'a> SSATransContext<'a> {
         for node in &self.cfg.nodes {
             let cfg_ptr = Arc::as_ptr(node);
             let dom_node = self.cfg_ptr2dom_node.get(&cfg_ptr).unwrap();
-            let frontiers = DomTree::domination_frontier(Arc::clone(&dom_node));
-
-            let node_cache = self.blk_cache.get(&cfg_ptr).unwrap();
-            let remote_live_out: HashSet<_> = node_cache.renamed_live_out.keys().cloned().collect();
+            let frontiers = DomTree::domination_frontier(Arc::clone(dom_node));
 
             for frontier_ptr in frontiers {
                 let cache = self.blk_cache.get_mut(&frontier_ptr).unwrap();
-                let original_live_in: HashSet<_> = cache.renamed_live_in.keys().cloned().collect();
-
-                for may_shadow in remote_live_out.intersection(&original_live_in) {
-                    cache.live_in_may_shadow.insert(may_shadow.clone());
-                }
+                cache.live_in_may_shadow.extend(cache.acc_live_in.clone());
             }
         }
     }
@@ -149,10 +177,11 @@ impl<'a> SSATransContext<'a> {
             let cfg_ptr = Arc::as_ptr(cfg_node);
             let cache = per_blk_cache.get(&cfg_ptr).unwrap();
 
+            // all the local renaming
             let mut to_rename: HashMap<_, _> = cache.renamed_live_in.clone();
 
             to_rename.iter_mut().for_each(|(var, new_name)| {
-                if !cache.live_in_may_shadow.contains(var) {
+                if !cache.live_in_may_shadow.contains_key(var) {
                     let propgated_name = non_ambiguious_var_renaming.get(var).cloned().unwrap();
                     *new_name = propgated_name;
                 }
@@ -175,8 +204,8 @@ impl<'a> SSATransContext<'a> {
 
             // insert get expr at the start of block
             let mut get_exprs = vec![];
-            for to_get in &cache.live_in_may_shadow {
-                let renamed = cache.renamed_live_in.get(to_get).unwrap();
+            for (to_get, from) in &cache.live_in_may_shadow {
+                let renamed = BlockSSATransContext::mangle_scheme(from, to_get.as_str(), 0);
                 let ty = cache.live_in_ty.get(to_get).unwrap();
                 get_exprs.push(
                     serde_json::from_str(&format!(
@@ -193,31 +222,44 @@ impl<'a> SSATransContext<'a> {
                 .blk
                 .instrs
                 .iter()
-                .position(|inst| !matches!(inst, LabelOrInst::Label { .. }))
-                .unwrap_or(0);
+                .position(|inst| {
+                    !matches!(inst, LabelOrInst::Label { .. })
+                    // handle the entry block may set case
+                })
+                .unwrap_or(1); // otherwise, basic block only contains a single label
+
             cfg_node_lock
                 .blk
                 .instrs
                 .splice(first_non_label_idx..first_non_label_idx, get_exprs);
 
             let mut merged_renamed_live_out = cache.renamed_live_out.clone();
-            for original_to_get_name in &cache.live_in_may_shadow {
+
+            // updating naming for get variable
+            for original_to_get_name in cache.live_in_may_shadow.keys() {
                 // if the live in we get is not shadowed
                 if !merged_renamed_live_out.contains_key(original_to_get_name) {
-                    let renamed_to_get = cache.renamed_live_in.get(original_to_get_name).unwrap();
-                    merged_renamed_live_out
-                        .insert(original_to_get_name.clone(), renamed_to_get.clone());
+                    let from = cache.acc_live_in.get(original_to_get_name).unwrap();
+                    merged_renamed_live_out.insert(
+                        original_to_get_name.clone(),
+                        BlockSSATransContext::mangle_scheme(
+                            from,
+                            original_to_get_name.as_str(),
+                            0,
+                        ),
+                    );
                 }
             }
-            non_ambiguious_var_renaming.extend(merged_renamed_live_out.into_iter());
+            non_ambiguious_var_renaming.extend(merged_renamed_live_out);
 
             let mut set_instrs = vec![];
             for succ in &cfg_node_lock.successors {
-                let succ_ptr = Weak::as_ptr(&succ);
+                let succ_ptr = Weak::as_ptr(succ);
                 let succ_cache = per_blk_cache.get(&succ_ptr).unwrap();
-                for to_set in &succ_cache.live_in_may_shadow {
+                for (to_set, from) in &succ_cache.live_in_may_shadow {
                     let to_set_local_name = non_ambiguious_var_renaming.get(to_set).unwrap();
-                    let remote_name = succ_cache.renamed_live_in.get(to_set).unwrap();
+                    let remote_name =
+                        BlockSSATransContext::mangle_scheme(from, to_set.as_str(), 0);
                     set_instrs.push(
                         serde_json::from_str(&format!(
                             r#"{{
@@ -251,11 +293,14 @@ impl<'a> SSATransContext<'a> {
                 );
             }
         }
-        let func_args: HashMap<_, _> = self.func_ctx.args.as_ref().map_or(HashMap::new(), |args| {
-            args.iter()
-                .map(|arg| (arg.name.clone(), arg.name.clone()))
-                .collect()
-        });
+
+        let func_args: HashMap<_, _> =
+            self.func_ctx.args.as_ref().map_or(HashMap::new(), |args| {
+                args.iter()
+                    .map(|arg| (arg.name.clone(), arg.name.clone()))
+                    .collect()
+            });
+
         preorder_recurse_on_dom_node(
             &self.dom_tree.root.upgrade().unwrap(),
             func_args,
@@ -293,7 +338,7 @@ impl<'a> BlockSSATransContext<'a> {
                         *arg = Self::mangle_scheme(&self.blk_label, arg, *next_to_assign - 1);
                     } else {
                         // live in vars
-                        let renamed = Self::mangle_scheme(&self.blk_label, &arg, 0);
+                        let renamed = Self::mangle_scheme(&self.blk_label, arg, 0);
                         self.renamed_live_in.insert(arg.clone(), renamed.clone());
                     }
                 });
@@ -310,7 +355,7 @@ impl<'a> BlockSSATransContext<'a> {
                         0
                     },
                 );
-                *dest = Self::mangle_scheme(&self.blk_label, &dest, *next_to_assign);
+                *dest = Self::mangle_scheme(&self.blk_label, dest, *next_to_assign);
                 *next_to_assign += 1;
             }
         }
@@ -365,7 +410,7 @@ impl WorkListAlgo for VarTypeAnalysis {
         in_flow
     }
 
-    fn merge(mut out_flow: Vec<Self::OutFlowType>) -> Self::InFlowType {
+    fn merge(out_flow: Vec<Self::OutFlowType>) -> Self::InFlowType {
         out_flow
             .into_iter()
             .reduce(|mut acc, next| {
@@ -379,5 +424,40 @@ impl WorkListAlgo for VarTypeAnalysis {
                 acc
             })
             .unwrap_or_default()
+    }
+}
+
+pub struct LivenessAnalysisWithLabelBackProp;
+
+impl WorkListAlgo for LivenessAnalysisWithLabelBackProp {
+    const FORWARD_PASS: bool = false;
+    type InFlowType = HashMap<String, String>;
+    type OutFlowType = HashMap<String, String>;
+
+    fn merge(out_flows: Vec<Self::OutFlowType>) -> Self::InFlowType {
+        out_flows.into_iter().flatten().collect()
+    }
+
+    fn transfer(&mut self, node: &NodeRef, in_flow: Option<Self::InFlowType>) -> Self::OutFlowType {
+        let node_lock = node.lock().unwrap();
+        let blk = &node_lock.blk;
+        let label = blk
+            .label
+            .as_ref()
+            .map_or("entry", |s| s.as_str())
+            .to_string();
+
+        let (able_to_kill, used_but_not_defed) = (blk.defs(), blk.used_but_not_defed());
+        let used_but_not_defed: HashMap<_, _> = used_but_not_defed
+            .into_iter()
+            .map(|var| (var, label.clone()))
+            .collect();
+        if let Some(mut in_flow) = in_flow {
+            in_flow.retain(|var, _| !able_to_kill.contains(var));
+            in_flow.extend(used_but_not_defed);
+            in_flow
+        } else {
+            used_but_not_defed
+        }
     }
 }
